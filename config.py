737009566e3d739d73696346d135c8def0bd472f
tommy-mor <thmorriss@gmail.com>
2021-02-22 19:00:24 +0000

    basic data class, about to start viewer

---
+++ config.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+from glue.core.component_id import ComponentID
+from glue.core.data import BaseCartesianData
+from glue.utils import view_shape
+import ete3
+
+
+# QUESTION
+# TODO Change this class to inherit from BaseData, so that it doesn't have to shim (?)
+# into Cartesian api
+# (second paragrpah of http://docs.glueviz.org/en/stable/developer_guide/data.html)
+class TreeData(BaseCartesianData):
+    def __init__(self):
+        super(TreeData, self).__init__()
+        self.data_cid = ComponentID(label='tree data componentid label', parent=self)
+        self.data = ete3.Tree("(A:1,(B:1,(E:1,D:1)Internal_1:0.5)Internal_2:0.5)Root;", format=1)
+    @property
+    def label(self):
+        return "Tree Data label"
+
+    @property
+    def shape(self):
+        return (42,)
+
+    @property
+    def main_components(self):
+        # only one component for now
+        return [self.data_cid]
+
+    def get_kind(self, cid):
+        # QUESTION
+        # one of 'numerical' => number?, 'categorical' => string, 'datetime' =>np.datetime64
+        #        , 'tree' = ete3.Tree ??
+
+        # http://etetoolkit.org/docs/latest/tutorial/tutorial_trees.html#reading-newick-trees
+        return 'tree'
+
+    def get_data(self, cid, view=None):
+        # note: view is a slice of a numpy array. does not really apply to tree data
+
+        if view is not None:
+            # QUESTION
+            return Exception('trees do not support numpy indexing')
+
+        #if cid in self.pixel_component_ids:
+            #return super(TreeData, self).get_data(cid, view=view)
+        #else:
+            #return np.random.random(view_shape(self.shape, view))
+
+        raise Exception('tree data is not numpy data')
+
+    def get_mask(self, subset_state, view=None):
+        return subset_state.to_mask(self, view=view)
+
+    def compute_statistic(self, statistic, cid,
+                          axis=None, finite=True,
+                          positive=False, subset_state=None,
+                          percentile=None, random_subset=None):
+
+        raise Exception('tree data statistic operations not yet defined')
+
+    def compute_histogram(self ,cid, range=None, bins=None, log=False,
+                          subset_state=None, subset_group=None):
+
+        raise Exception('tree data histogram not yet defined')
+
+from glue.core import DataCollection
+from glue.app.qt.application import GlueApplication
+
+d = TreeData()
+dc = DataCollection([d])
+ga = GlueApplication(dc)
+ga.start()
---
+++ viewer.py
@@ -0,0 +1,2 @@
+# TODO make it possible to load tree data using a factory
+# TODO make it possible to view using ete
