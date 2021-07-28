6f164b8995813591d6556747f7719cbce55b009f
tommy-mor <thmorriss@gmail.com>
2021-07-27 17:23:40 -0700

    added way to link tree indexes from ui :)

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ import ete3
 import viewer
 from glue import qglue
 from glue.core.data_factories.helpers import has_extension
-from glue.config import data_factory, link_function
+from glue.config import data_factory, link_function, link_helper
+from glue.core.link_helpers import LinkCollection
 
 
 # linking info
@@ -35,6 +36,8 @@ def tree_process_bad(fname):
     tree = ete3.Tree(fname, format=1)
     data = TreeData(tree)
     return data
+
+# {A} goal: make class that can be put into UserDataWrapper spot for the ui so it can be added by helper
 
 
 class TreeData(Data):
@@ -123,3 +126,31 @@ def read_newick(fname):
 
 
 # https://github.com/glue-viz/glue/blob/241edb32ab6f4a82adf02ef3711c16342fd214ed/glue/plugins/dendro_viewer/qt/data_viewer.py#L92
+
+
+@link_helper(category='Tree Viewer')
+class Link_Index_By_Value(LinkCollection):
+    # inherit from linkCollection to skip this line https://github.com/glue-viz/glue/blob/5a878451a1636b141a687a482239a37287a32198/glue/config.py#L790
+    cid_independent = False
+
+    display = 'Link By Value'
+    description = 'WARNING: once you close this dialog, this link can only be removed by restarting glue'
+
+    labels1 = ['first value column']
+    labels2 = ['second value column']
+
+    _links = []
+
+    def __init__(self, *args, cids1=None, cids2=None, data1=None, data2=None):
+        # only support linking by one value now, even tho link_by_value supports multiple
+        assert len(cids1) == 1
+        assert len(cids2) == 1
+
+        self.data1 = data1
+        self.data2 = data2
+        self.cids1 = cids1
+        self.cids2 = cids2
+
+        data1.join_on_key(data2, cids1[0], cids2[0])
+
+# based on https://sourcegraph.com/github.com/glue-viz/glue/-/blob/glue/plugins/coordinate_helpers/link_helpers.py?L42:33
