dad015c72e848c5366ff0f68b31f88cf3560a6fe
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2021-10-06 14:32:43 -0400

    Finish migration into main glue

--- glue_tree_viewer/__init__.py
+++ glue_tree_viewer/__init__.py
@@ -1,6 +1,5 @@
 from .viewer import HomeButton, ZoomOut, PointSelect, AnimationTool, LineSelect, PanTool# noqa
 from .viewer import TreeDataViewer #noqa
-from .utils import Link_Index_By_Value
 #from .my_actions import T_NodeActions
 
 def setup():
