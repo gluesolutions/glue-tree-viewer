00c53cd72d9ac96d016e62fe24815e0f0ee23f72
tommy-mor <thmorriss@gmail.com>
2021-10-07 16:51:39 -0400

    included data loaders so you can load trees from ui

--- glue_tree_viewer/__init__.py
+++ glue_tree_viewer/__init__.py
@@ -2,7 +2,8 @@ from .viewer import HomeButton, ZoomOut, PointSelect, AnimationTool, LineSelect,
 from .viewer import TreeDataViewer #noqa
 #from .my_actions import T_NodeActions
 
+from .utils import read_dendro, read_newick
+
 def setup():
     from glue.config import qt_client
     qt_client.add(TreeDataViewer)
-
